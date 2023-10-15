23f6aff244f7a5554bfaa66f9059e2a481e87835
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-10-15 16:08:18 +0200

    add fonts and set container sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./resources/views/**/*.edge'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+        },
+      },
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+        cherry: ['Cherry Bomb One', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
