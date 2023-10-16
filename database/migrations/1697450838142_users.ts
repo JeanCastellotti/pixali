d21e3ff1b060539cd4e96e2fdc62e74f4d878a09
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-10-16 12:21:22 +0200

    add and configure @adonisjs/auth

---
+++ database/migrations/1697450838142_users.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('username', 10).notNullable().unique()
+      table.string('email', 255).notNullable().unique()
+      table.string('password', 180).notNullable()
+      table.string('remember_me_token').nullable()
+      table.string('password_reset_token').nullable()
+      table.timestamp('email_verified_at', { useTz: true }).nullable()
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
