c403674ffc8ca40f11621b8c1b57e59f8c03ef9b
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-10-21 17:00:18 +0200

    test: add login tests

---
+++ tests/functional/auth/login.spec.ts
@@ -0,0 +1,72 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { test } from '@japa/runner'
+import User from 'App/Models/User'
+
+test.group('Auth login', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('should login a user', async ({ client }) => {
+    const user = await User.create({
+      username: 'test',
+      email: 'test@email.io',
+      password: 'Password!1234',
+    })
+
+    const response = await client.post('/login').withCsrfToken().form({
+      email: user.email,
+      password: 'Password!1234',
+    })
+
+    console.log(response.flashMessages())
+
+    response.assertStatus(200)
+    response.assertRedirectsToRoute('home')
+  })
+
+  test('should fail to login with invalid credentials', async ({ client }) => {
+    const user = await User.create({
+      username: 'test',
+      email: 'test@email.io',
+      password: 'Password!1234',
+    })
+
+    const response = await client.post('/login').header('Referer', '/login').withCsrfToken().form({
+      email: user.email,
+      password: 'Password!12345',
+    })
+
+    response.assertStatus(200)
+    response.assertRedirectsToRoute('auth.create')
+  })
+
+  test('should redirect authenticated user', async ({ client }) => {
+    const user = await User.create({
+      username: 'test',
+      email: 'test@email.io',
+      password: 'Password!1234',
+    })
+
+    const response = await client.get('/login').loginAs(user)
+
+    response.assertRedirectsToRoute('home')
+  })
+
+  test('should fail with invalid payload', async ({ client }) => {
+    const user = await User.create({
+      username: 'test',
+      email: 'test@email.io',
+      password: 'Password!1234',
+    })
+
+    const response = await client.post('/login').header('Referer', '/login').withCsrfToken().form({
+      username: 'test',
+      password: 'Password!1234',
+    })
+
+    response.assertStatus(200)
+    response.assertRedirectsToRoute('auth.create')
+  })
+})
