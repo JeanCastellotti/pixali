00e21c8668d0f8eceaf4c7d2174d7fb3b933758a
Jean Castellotti <castellotti.j@gmail.com>
2024-05-14 14:11:27 +0200

    add favicon and rubik font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./resources/views/app.edge', './inertia/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
