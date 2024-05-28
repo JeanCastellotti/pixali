d1d1734a7446898835b906d7e873563493d7cdb3
Jean Castellotti <castellotti.j@gmail.com>
2024-05-28 17:16:02 +0200

    update validator rules for user creation

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -6,6 +6,8 @@ export const createUserValidator = vine.compile(
       .string()
       .trim()
       .alpha()
+      .minLength(3)
+      .maxLength(12)
       .unique(async (db, value) => {
         const user = await db.from('users').where('username', value).first()
         return !user
@@ -26,9 +28,11 @@ export const createUserValidator = vine.compile(
 createUserValidator.messagesProvider = new SimpleMessagesProvider({
   'username.required': "Le nom d'utilisateur est obligatoire",
   'username.alpha': "Le nom d'utilisateur ne peut contenir que des lettres",
+  'username.minLength': "Le nom d'utilisateur doit faire au moins 3 caractères",
+  'username.maxLength': "Le nom d'utilisateur ne doit pas dépasser 12 caractères",
   'username.database.unique': "Le nom d'utilisateur est indisponible",
   'email.required': "L'adresse e-mail est obligatoire",
-  'email': "L'adresse e-mail n'est pas valide",
+  'email': "Le format de l'adresse e-mail est incorrect",
   'email.database.unique': "L'adresse e-mail est indisponible",
   'password.required': 'Le mot de passe est obligatoire',
   'password.minLength': 'Le mot de passe doit faire au moins 8 caractères',
