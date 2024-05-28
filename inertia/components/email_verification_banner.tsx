ba3e33bc2a99a6d8c60a10c5b2f5b9bc5f06abbe
Jean Castellotti <castellotti.j@gmail.com>
2024-05-28 17:16:45 +0200

    update email verification banner styles

--- inertia/components/email_verification_banner.tsx
+++ inertia/components/email_verification_banner.tsx
@@ -18,17 +18,17 @@ function EmailVerificationBanner() {
   if (user.emailVerifiedAt) return
 
   return (
-    <div className="bg-amber-500 py-3">
-      <div className="container flex items-center justify-center gap-6 text-sm text-amber-50">
-        <ShieldExclamationIcon className="size-6" />
-        <span>Votre adresse e-mail n'a pas été vérifiée.</span>
+    <div className="bg-slate-800 py-3">
+      <div className="container flex items-center justify-center gap-6">
+        <ShieldExclamationIcon className="size-6 fill-slate-100" />
+        <span className="text-sm text-slate-100">Votre adresse e-mail n'a pas été vérifiée.</span>
         <Link
           href="/verify"
           as="button"
           method="post"
-          className="rounded border-2 border-transparent bg-amber-50 px-2.5 py-1.5 text-amber-500 transition hover:opacity-80"
+          className="rounded bg-slate-100 px-2.5 py-1.5 text-xs text-slate-800 transition hover:opacity-80"
         >
-          Envoyer un nouveau lien
+          <span>Envoyer un nouveau lien</span>
         </Link>
       </div>
     </div>
