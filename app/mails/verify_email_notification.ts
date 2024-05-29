3de5f3a48d8ed3b3163d98c0813e0bd227dc8a72
Jean Castellotti <castellotti.j@gmail.com>
2024-05-29 12:12:51 +0200

    update email verification URL creation

--- app/mails/verify_email_notification.ts
+++ app/mails/verify_email_notification.ts
@@ -11,13 +11,13 @@ export default class VerifyEmailNotification extends BaseMail {
   }
 
   prepare() {
-    const signedURL = router.makeSignedUrl(
-      'email.verify',
-      { email: this.user.email },
-      { expiresIn: '30m' }
-    )
-
-    const emailVerificationURL = env.get('APP_URL') + signedURL
+    const emailVerificationURL = router
+      .builder()
+      .prefixUrl(env.get('APP_URL'))
+      .params({ email: this.user.email })
+      .makeSigned('email.verify', {
+        expiresIn: '30m',
+      })
 
     this.message.to(this.user.email)
     this.message.htmlView('emails/verify_email', {
