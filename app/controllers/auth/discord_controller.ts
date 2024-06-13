41275528b8e9f1fb998f15d13771fc7d4e9f3eea
Jean Castellotti <castellotti.j@gmail.com>
2024-06-13 13:24:39 +0200

    minor updates to discord controller

--- app/controllers/auth/discord_controller.ts
+++ app/controllers/auth/discord_controller.ts
@@ -1,40 +1,36 @@
-import Profile from '#models/profile'
 import User from '#models/user'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class DiscordController {
   redirect({ ally }: HttpContext) {
-    return ally.use('discord').redirect((request) => {
-      request.scopes(['email', 'identify'])
-    })
+    return ally.use('discord').redirect()
   }
 
   async callback({ ally, auth, response, session }: HttpContext) {
     const discord = ally.use('discord')
 
     if (discord.accessDenied()) {
-      session.flash('error', "Vous avez annulé le processus d'identification")
-
+      session.flash('error', "Vous avez annulé le processus d'identification.")
       return response.redirect().toRoute('session.create')
     }
 
-    if (discord.stateMisMatch() || discord.hasError()) {
-      session.flash('error', 'Un problème est survenu. Veuillez réessayer.')
+    if (discord.stateMisMatch()) {
+      session.flash('error', 'La requête a expiré. Veuillez réessayer.')
+      return response.redirect().toRoute('session.create')
+    }
 
+    if (discord.hasError()) {
+      session.flash('error', "Une erreur inattendue s'est produite.")
       return response.redirect().toRoute('session.create')
     }
 
     const discordUser = await discord.user()
 
-    // return discordUser
+    const userFound = await User.query().where('discord_id', discordUser.id).first()
 
-    let user = await User.query().where('discord_id', discordUser.id).first()
-
-    if (user) {
-      await auth.use('web').login(user)
-
+    if (userFound) {
+      await auth.use('web').login(userFound)
       session.flash('success', 'Vous êtes connecté.')
-
       return response.redirect().toRoute('home')
     }
 
@@ -44,12 +40,11 @@ export default class DiscordController {
       .first()
 
     if (userExists) {
-      session.flash('info', 'Vous avez déjà un compte avec cette adresse e-mail.')
-
+      session.flash('info', 'Cet e-mail est déjà lié à un compte.')
       return response.redirect().toRoute('session.create')
     }
 
-    user = await User.create({
+    const newUser = await User.create({
       discordId: discordUser.id,
       username: discordUser.nickName,
       email: discordUser.email,
@@ -57,11 +52,9 @@ export default class DiscordController {
       emailVerified: discordUser.emailVerificationState === 'verified',
     })
 
-    const profile = new Profile()
-    await user.related('profile').save(profile)
+    await newUser.related('profile').create({})
 
-    await auth.use('web').login(user)
-
+    await auth.use('web').login(newUser)
     session.flash('success', 'Vous êtes connecté.')
 
     return response.redirect().toRoute('home')
